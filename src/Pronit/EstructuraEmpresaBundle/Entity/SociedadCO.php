9a1cf80c20e44e6284eb5dcb64ef66795a62ac95
ldelia <lisandro.delia@gmail.com>
2014-02-04 19:33:46 -0300

    ADD Admin para sociedad FI, GL y CO

--- src/Pronit/EstructuraEmpresaBundle/Entity/SociedadCO.php
+++ src/Pronit/EstructuraEmpresaBundle/Entity/SociedadCO.php
@@ -7,23 +7,10 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * 
  *
- * @author gcaseres
+ * @author ldelia
  * @ORM\Entity
- * @ORM\Table(name="eempre_sociedadesco")
  */
 class SociedadCO extends Sociedad
-{
-    /**
-     * @ORM\Column(type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */    
-    private $id;
-    
-    public function getId()
-    {
-        return $this->id;
-    }
-    
+{    
 }
 
