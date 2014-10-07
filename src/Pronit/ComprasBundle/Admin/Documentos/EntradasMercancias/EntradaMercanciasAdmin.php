fc4a168793dc64c451aa24bea56e4caf80026a0e
ldelia <lisandro.delia@gmail.com>
2014-10-07 15:33:27 -0300

    Avances en la Transacción de Entrada de Mercancias

--- src/Pronit/ComprasBundle/Admin/Documentos/EntradasMercancias/EntradaMercanciasAdmin.php
+++ src/Pronit/ComprasBundle/Admin/Documentos/EntradasMercancias/EntradaMercanciasAdmin.php
@@ -4,7 +4,10 @@ namespace Pronit\ComprasBundle\Admin\Documentos\EntradasMercancias;
 use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
+use Knp\Menu\ItemInterface as MenuItemInterface;
+use Sonata\AdminBundle\Admin\AdminInterface;
 use Sonata\AdminBundle\Route\RouteCollection;
+use Sonata\AdminBundle\Show\ShowMapper;
 
 use Pronit\ComprasBundle\Entity\Documentos\EntradasMercancias\EntradaMercancias;
 use Pronit\ComprasBundle\Entity\Documentos\EntradasMercancias\ItemEntradaMercancias;
@@ -21,30 +24,57 @@ class EntradaMercanciasAdmin extends Admin
     protected function configureListFields(ListMapper $listMapper)
     {
         $listMapper
-            ->addIdentifier('numero')
+            ->addIdentifier('numero', null, array('route' => array('name' => 'show')))
+            ->add('estadoCompras')
             ->add('estado')
             ->add('sociedad')
             ->add('fecha', 'date', array( 'format' => 'd/m/Y' ))
             ->add('proveedorSociedad', null, array('label'=>'Proveedor') ) 
             ->add('centroLogistico')
             ->add('moneda')
-            ->add('_action', 'actions', array(
-                    'actions' => array(
-                        'show' => array(),
-                        'edit' => array(),
-                    ),                
-                )
-            )                
         ;
     }
     
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        // Here we set the fields of the ShowMapper variable, $showMapper (but this can be called anything)
+        $showMapper
+            ->with('Cabecera')
+                ->add('numero')
+                ->add('estadoCompras')
+                ->add('estado')
+                ->add('sociedad')                
+                ->add('fecha', 'date', array( 'format' => 'd/m/Y' ))                                                
+                ->add('proveedorSociedad', null, array('label'=>'Proveedor') )                  
+                ->add('centroLogistico')
+                ->add('moneda')                
+                ->add('textoCabecera')
+            ->end()      
+            ->with('Items')
+                ->add('items', null, array('template' => 'PronitComprasBundle:Documentos\Pedidos\show:items.html.twig')) /* todo fixme */
+            ->end()  
+
+        ;
+    }        
+    
     protected function configureFormFields(FormMapper $formMapper)
     {
+        $pedido_id = $this->request->query->get('pedido_id');
+
+        $formMapper
+            ->with('Entrada de Mercancías', array('class' => 'col-md-6'))->end()
+            ->with('Cabecera', array('class' => 'col-md-6'))->end()
+            ->with('Items', array('class' => 'col-md-12') )->end()
+        ;   
+        
         $formMapper
             ->with('Entrada de Mercancías')
                 ->add('numero')
-                ->add('sociedad')                
                 ->add('fecha', 'date', array('widget' => 'single_text'))                                                
+                ->add('pedido_id', 'hidden', array('data' => $pedido_id, 'mapped' => false)) 
+                ->add('sociedad')                
+            ->end()    
+            ->with('Cabecera')
                 ->add('proveedorSociedad', null, array('label'=>'Proveedor') )
                 ->add('centroLogistico')
                 ->add('moneda')
@@ -67,17 +97,12 @@ class EntradaMercanciasAdmin extends Admin
         ; 
     }        
     
-    protected function configureRoutes(RouteCollection $collection)
-    {
-        $collection->clearExcept(array('create' , 'list'));        
-    }    
-    
     public function getNewInstance()
     {
         $entradaMercancia = new EntradaMercancias();
         
         $pedido_id = $this->request->query->get('pedido_id');
-        
+       
         /* @var $pedido \Pronit\ComprasBundle\Entity\Documentos\Pedidos\Pedido  */
         $pedido = $this->getModelManager()->find('Pronit\ComprasBundle\Entity\Documentos\Pedidos\Pedido', $pedido_id);        
         
@@ -96,12 +121,47 @@ class EntradaMercanciasAdmin extends Admin
                 $item->setClasificador($itemPedido->getClasificador());        
                 $item->setBienServicio($itemPedido->getBienServicio());
                 $item->setPrecioUnitario($itemPedido->getPrecioUnitario());
-                $item->setCantidad($itemPedido->getCantidad());        
+                $item->setCantidad($itemPedido->getCantidadPendienteDeEntrega());        
                 $item->setEscala($itemPedido->getEscala());
+                $item->setReferenciaItemPedido( $itemPedido );
 
                 $entradaMercancia->addItem($item);
             }
         }
         return $entradaMercancia;
     }
+    
+    protected function configureSideMenu(MenuItemInterface $menu, $action, AdminInterface $childAdmin = null) 
+    {        
+        if( $action == "show"){
+            
+            $admin = $this->isChild() ? $this->getParent() : $this;
+            $id = $admin->getRequest()->get('id');
+
+            /* @var $entradaMercancias \Pronit\ComprasBundle\Entity\Documentos\EntradasMercancias\EntradaMercancias  */            
+            $entradaMercancias = $this->getObject($id);
+            
+            if( $entradaMercancias->isModificable() ){
+                
+                $menu->addChild( 'Contabilizar', array('uri' => $admin->generateUrl('contabilizar', array('id' => $id))) )
+                        ->setLinkAttribute('class', 'glyphicon glyphicon-ok');                
+            }            
+        }
+    }
+    
+    public function isGranted($name, $entradaMercancias = null)
+    {
+        /* @var $entradaMercancias \Pronit\ComprasBundle\Entity\Documentos\EntradasMercancias\EntradaMercancias  */            
+        
+        if (( $name == 'EDIT' ) && ( ! is_null($entradaMercancias)) ){            
+            return $entradaMercancias->isModificable() && parent::isGranted($name, $entradaMercancias);
+        }else{
+            return parent::isGranted($name, $entradaMercancias);
+        }        
+    }
+    
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection->add('contabilizar', $this->getRouterIdParameter() . '/contabilizar');
+    }        
 }
