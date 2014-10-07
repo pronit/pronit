157e72c21edd1b8833cffea724dca0e37e307187
ldelia <lisandro.delia@gmail.com>
2014-10-07 15:32:49 -0300

    Avances en la Transacción de Entrada de Mercancias

--- src/Pronit/ComprasBundle/Entity/Documentos/Pedidos/ItemPedido.php
+++ src/Pronit/ComprasBundle/Entity/Documentos/Pedidos/ItemPedido.php
@@ -5,6 +5,12 @@ namespace Pronit\ComprasBundle\Entity\Documentos\Pedidos;
 use Doctrine\ORM\Mapping as ORM;
 
 use Pronit\ComprasBundle\Entity\Documentos\ItemAbastecimientoExterno;
+use Pronit\ComprasBundle\Entity\Documentos\EntradasMercancias\ItemEntradaMercancias;
+
+use Pronit\ComprasBundle\Entity\Documentos\Pedidos\Estados\EstadoPedido;
+use Pronit\ComprasBundle\Entity\Documentos\Pedidos\Estados\SinEntregar;
+use Pronit\ComprasBundle\Entity\Documentos\Pedidos\Estados\Finalizado;
+use Pronit\ComprasBundle\Entity\Documentos\Pedidos\Estados\EntregadoParcialmente;
 
 /**
  *
@@ -13,9 +19,69 @@ use Pronit\ComprasBundle\Entity\Documentos\ItemAbastecimientoExterno;
 */
 class ItemPedido extends ItemAbastecimientoExterno
 {
+    /**
+     * @ORM\OneToOne(targetEntity="Pronit\ComprasBundle\Entity\Documentos\Pedidos\Estados\EstadoPedido", cascade={"persist", "remove"}, orphanRemoval=true)
+     **/    
+    protected $estado;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="Pronit\ComprasBundle\Entity\Documentos\EntradasMercancias\ItemEntradaMercancias", mappedBy="referenciaItemPedido", cascade={"ALL"})
+     */
+    protected $referenciasItemEntradaMercancias;    
+    
+    public function __construct()
+    {
+        parent::__construct();
+        
+        $this->referenciasItemEntradaMercancias = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->setEstado( new SinEntregar() );
+    }
+    
     public function __toString()
     {
-        return $this->getId();
+        return (string) $this->getId();
+    }
+
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    protected function setEstado(EstadoPedido $estado)
+    {
+        $this->estado = $estado;
+    }    
+    
+    function getReferenciasItemEntradaMercancias()
+    {
+        return $this->referenciasItemEntradaMercancias;
+    }
+
+    function addReferenciaItemEntradaMercancias( ItemEntradaMercancias $itemEntradaMercancias )
+    {
+        $this->referenciasItemEntradaMercancias[] = $itemEntradaMercancias;
+    }
+    
+    public function registrarEntradaMercancias( ItemEntradaMercancias $itemEntradaMercancias )
+    {
+        if( $this->getCantidadPendienteDeEntrega() > 0 ){
+            $this->setEstado( new EntregadoParcialmente() );
+        }else{
+            $this->setEstado( new Finalizado() );
+        }        
+    }
+    
+    public function getCantidadPendienteDeEntrega()
+    {
+        $entregado = 0;
+        
+        /* @var $itemEntradaMercancias \Pronit\ComprasBundle\Entity\Documentos\EntradasMercancias\ItemEntradaMercancias */
+        foreach( $this->getReferenciasItemEntradaMercancias() as $itemEntradaMercancias )
+        {
+            $entregado = $entregado + $itemEntradaMercancias->getCantidad();
+        }
+        
+        return $this->getCantidad() - $entregado;
     }
 }
 
