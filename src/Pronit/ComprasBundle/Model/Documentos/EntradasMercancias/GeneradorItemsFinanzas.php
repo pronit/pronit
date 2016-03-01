d9ba1e018c57845b190f2a448903a21a6fe5f564
gcaseres <gcaseres@gmail.com>
2016-03-01 18:42:03 -0300

    Ahora el generador de item finanzas de la entrada de mercancías utiliza el aspecto "imputaobjetocostos" para determinar que items finanzas realizaran imputaciones a objetos de costos.

--- src/Pronit/ComprasBundle/Model/Documentos/EntradasMercancias/GeneradorItemsFinanzas.php
+++ src/Pronit/ComprasBundle/Model/Documentos/EntradasMercancias/GeneradorItemsFinanzas.php
@@ -4,22 +4,22 @@ namespace Pronit\ComprasBundle\Model\Documentos\EntradasMercancias;
 
 use Doctrine\ORM\EntityManager;
 use Exception;
+use Pronit\ComprasBundle\Entity\Documentos\EntradasMercancias\EntradaMercancias;
+use Pronit\ComprasBundle\Entity\Documentos\EntradasMercancias\ItemEntradaMercancias;
+use Pronit\CoreBundle\Entity\Documentos\ClaseDocumento;
 use Pronit\CoreBundle\Entity\Documentos\Documento;
 use Pronit\CoreBundle\Entity\Documentos\Item;
-use Pronit\ComprasBundle\Entity\Documentos\EntradasMercancias\ItemEntradaMercancias;
-use Pronit\CoreBundle\Entity\Documentos\ItemFinanzas;
-use \Pronit\CoreBundle\Entity\Documentos\ItemFinanzasEntradaMercancias;
+use Pronit\CoreBundle\Entity\Documentos\ItemFinanzasEntradaMercancias;
+use Pronit\CoreBundle\Entity\Operaciones\Operacion;
+use Pronit\CoreBundle\Model\Aspectos\IAspectoManager;
 use Pronit\CoreBundle\Model\Contabilidad\Customizing\IImputacionesCustomizingManager as FIIImputacionesCustomizingManager;
 use Pronit\CoreBundle\Model\Documentos\IGeneradorItemsFinanzas;
+use Pronit\CoreBundle\Model\Operaciones\Contextos\Documentos\ContextoDocumentoEntradaMercancias;
 use Pronit\CoreBundle\Model\Operaciones\Contextos\Documentos\ContextoItemDocumentoEntradaMercancias;
+use Pronit\CustomizingBundle\Entity\Operaciones\MappingClaseDocumentoOperacion;
 use Pronit\CustomizingBundle\Entity\Operaciones\MappingClasificadorItemOperacion;
 use Pronit\CustomizingBundle\Model\Operaciones\IOperacionesCustomizingManager;
 use Pronit\GestionBienesYServiciosBundle\Model\Customizing\Contabilidad\IImputacionesCustomizingManager as MMIImputacionesCustomizingManager;
-use Pronit\CoreBundle\Entity\Documentos\ClaseDocumento;
-use \Pronit\CoreBundle\Model\Operaciones\Contextos\Documentos\ContextoDocumentoEntradaMercancias;
-use Pronit\CoreBundle\Entity\Operaciones\Operacion;
-use Pronit\CustomizingBundle\Entity\Operaciones\MappingClaseDocumentoOperacion;
-use Pronit\ComprasBundle\Entity\Documentos\EntradasMercancias\EntradaMercancias;
 
 /**
  * Description of GeneradorItemsFinanzas
@@ -28,16 +28,22 @@ use Pronit\ComprasBundle\Entity\Documentos\EntradasMercancias\EntradaMercancias;
  */
 class GeneradorItemsFinanzas implements IGeneradorItemsFinanzas {
 
-    protected $operacionesCustomizingManager;
-    protected $mmImputacionesCustomizingManager;
-    protected $fiImputacionesCustomizingManager;
-    protected $em;
+    /**
+     *
+     * @var IAspectoManager
+     */
+    private $imputaObjetoCostosManager;
+    private $operacionesCustomizingManager;
+    private $mmImputacionesCustomizingManager;
+    private $fiImputacionesCustomizingManager;
+    private $em;
 
-    public function __construct(EntityManager $em, IOperacionesCustomizingManager $operacionesCustomizingManager, MMIImputacionesCustomizingManager $mmImputacionesCustomizingManager, FIIImputacionesCustomizingManager $fiImputacionesCustomizingManager) {
+    public function __construct(EntityManager $em, IOperacionesCustomizingManager $operacionesCustomizingManager, MMIImputacionesCustomizingManager $mmImputacionesCustomizingManager, FIIImputacionesCustomizingManager $fiImputacionesCustomizingManager, IAspectoManager $imputaObjetoCostosManager) {
         $this->em = $em;
         $this->operacionesCustomizingManager = $operacionesCustomizingManager;
         $this->mmImputacionesCustomizingManager = $mmImputacionesCustomizingManager;
         $this->fiImputacionesCustomizingManager = $fiImputacionesCustomizingManager;
+        $this->imputaObjetoCostosManager = $imputaObjetoCostosManager;
     }
 
     public function generar(Documento $documento) {
@@ -52,40 +58,37 @@ class GeneradorItemsFinanzas implements IGeneradorItemsFinanzas {
         $this->generarDesdeEntradaMercancias($documento);
     }
 
-    protected function generarDesdeItem(Item $item) 
-    {
+    protected function generarDesdeItem(Item $item) {
         $this->generarDesdeItemEntradaMercancias($item);
     }
-    
-    protected function generarDesdeEntradaMercancias(EntradaMercancias $entradaMercancias)
-    {        
+
+    protected function generarDesdeEntradaMercancias(EntradaMercancias $entradaMercancias) {
         $claseDocumento = $this->em->getRepository('Pronit\CoreBundle\Entity\Documentos\ClaseDocumento')->find(ClaseDocumento::CODIGO_ENTRADAMERCANCIAS);
         /* @var $claseDocumento ClaseDocumento */
-        
+
         $mappings = $this->operacionesCustomizingManager->getMappingsByClaseDocumento($claseDocumento);
-        
-        foreach($mappings as $mapping){
+
+        foreach ($mappings as $mapping) {
             /* @var $mapping MappingClaseDocumentoOperacion */
-            
-            /* @var $operacion Operacion */    
+
+            /* @var $operacion Operacion */
             $operacion = $mapping->getOperacion();
-            $funcion = $mapping->getFuncion();            
-            $cuenta = $this->fiImputacionesCustomizingManager->getCuenta($operacion);                        
-            
+            $funcion = $mapping->getFuncion();
+            $cuenta = $this->fiImputacionesCustomizingManager->getCuenta($operacion);
+
             $contexto = new ContextoDocumentoEntradaMercancias($operacion, $entradaMercancias);
             $importe = $funcion->ejecutar($contexto);
 
-            if($importe != 0){
-                $itemFinanzas = new ItemFinanzasEntradaMercancias($operacion, $cuenta);                
+            if ($importe != 0) {
+                $itemFinanzas = new ItemFinanzasEntradaMercancias($operacion, $cuenta);
                 $itemFinanzas->setImporte($importe);
-                
+
                 $entradaMercancias->addItemFinanzas($itemFinanzas);
             }
         }
     }
-    
-    protected function generarDesdeItemEntradaMercancias(ItemEntradaMercancias $item) 
-    {    
+
+    protected function generarDesdeItemEntradaMercancias(ItemEntradaMercancias $item) {
         $documento = $item->getDocumento();
         $clasificador = $item->getClasificador();
         $mappingsClasificadorItemOperacion = $this->operacionesCustomizingManager->getMappingsByClasificadorItem($clasificador);
@@ -106,13 +109,12 @@ class GeneradorItemsFinanzas implements IGeneradorItemsFinanzas {
             $importe = $funcion->ejecutar($contexto);
 
             if ($importe != 0) {                
-                
-                if( is_null($item->getObjetoCosto()) ){
-                    $itemFinanzas = new ItemFinanzasEntradaMercancias( $operacion, $cuenta);    
-                }else{
-                    $itemFinanzas = new ItemFinanzasEntradaMercancias( $operacion, $cuenta, $item->getObjetoCosto() );
+                if (!is_null($item->getObjetoCosto()) && $this->imputaObjetoCostosManager->has($operacion)) {
+                    $itemFinanzas = new ItemFinanzasEntradaMercancias($operacion, $cuenta, $item->getObjetoCosto());
+                } else {                    
+                    $itemFinanzas = new ItemFinanzasEntradaMercancias($operacion, $cuenta);
                 }
-                
+
                 $itemFinanzas->setImporte($importe);
                 $documento->addItemFinanzas($itemFinanzas);
             }
